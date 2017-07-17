a7d205577e7e0f1c0f7c57463832892d20aab6da
rylo5688 <loi.ryan18@gmail.com>
2017-07-17 15:36:50 -0600

    testing menubar option, still do not have it working yet

--- test.py
+++ test.py
@@ -1,4 +1,5 @@
-import Tkinter
+from Tkinter import *
+from tkFileDialog import askopenfilename
 
 def onKeyPress(event):
     text.insert('insert', '%s' % (event.char, ))
@@ -16,42 +17,64 @@ class createPage(object):
     def saveExit(object): #runs when the save and exit button is clicked
         newContents = object.text.get(0.0, 'end') #gets all the contents from the text widget
         uFile = open(object.file + ".txt", "w+") #opens the file in writing mode
+
         #add functionality where user saves as a new file
         #if they didn't open one to begin with
         uFile.write(newContents) #writing out the contents from the text widget to the file
         uFile.close()
         object.page.destroy() #closes the text widget
 
+    def openfile(self):
+        ftypes = [('Python files', '*.py'), ('All files', '*')]
+        dlg = tkFileDialog.Open(self, filetypes = ftypes)
+        fl = dlg.show()
+
+        if fl != '':
+            text = self.readFile(fl)
+            self.txt.insert(END, text)
+
     def __init__(self, fileName):
-        self.page = Tkinter.Tk()
-        self.page.title(fileName)
+        self.page = Tk()
+        self.page.title("test") #self.page.title(fileName)
+        #self.page.pack(fill=BOTH, expand=1)
         self.page.geometry('450x450')
+
         #containter for the button
-        self.page.buttonframe = Tkinter.Frame(self.page)
-        self.page.buttonframe.grid(row=0, column=1)
+        #self.page.buttonframe = Tkinter.Frame(self.page)
+        #self.page.buttonframe.grid(row=0, column=1)
 
-        #save button
-        self.bSave = Tkinter.Button(self.page.buttonframe, text="Save", command=self.save).grid(row=0, column=0)
+        #File button
+        self.page.menuBar = Menu(self.page)
+        self.fileMenu = Menu(self.page.menuBar, tearoff=0)
+        self.fileMenu.add_command(label="Open", command=self.openfile)
+        self.fileMenu.add_command(label="Save", command=self.save)
+        self.fileMenu.add_command(label="Save and Exit", command=self.saveExit)
+        self.fileMenu.add_separator()
 
-        #save and exit button
-        self.bSaveAndExit = Tkinter.Button(self.page.buttonframe, text="Save and Exit", command=self.saveExit).grid(row=0,column=1)
+        self.page.menuBar.add_cascade(label="File", menu=self.fileMenu)
+        # #Save button
+        # self.bSave = Tkinter.Button(self.page.buttonframe, text="Save", command=self.save).grid(row=0, column=0)
+        #
+        # #Save and exit button
+        # self.bSaveAndExit = Tkinter.Button(self.page.buttonframe, text="Save and Exit", command=self.saveExit).grid(row=0,column=1)
+        #
+        # self.page.buttonframe.pack(fill='x')
+        #
+        # self.text = Tkinter.Text(self.page, background='white', foreground='black', font=('Comic Sans MS', 12))
+        #
+        # #later add functionality for only opening
+        # #when there's a specified file to open
+        # uFileContents = "" #contents already in the file
+        # self.uFile = open(fileName + ".txt", "r+")#opening a known file
+        # for line in self.uFile: #getting the contents from the file
+        #     uFileContents += line
+        # self.file = fileName
+        # self.uFile.close()
+        #
+        # self.text.insert(0.0, uFileContents) #opens text widget with the text from the file
+        # self.text.pack()
 
-        self.page.buttonframe.pack(fill='x')
-
-        self.text = Tkinter.Text(self.page, background='white', foreground='black', font=('Comic Sans MS', 12))
-
-        #later add functionality for only opening
-        #when there's a specified file to open
-        uFileContents = "" #contents already in the file
-        self.uFile = open(fileName + ".txt", "r+")#opening a known file
-        for line in self.uFile: #getting the contents from the file
-            uFileContents += line
-        self.file = fileName
-        self.uFile.close()
-
-        self.text.insert(0.0, uFileContents) #opens text widget with the text from the file
-        self.text.pack()
-
+        self.page.config(menu=self.page.menuBar)
         self.page.mainloop()
         self.page.bind('<KeyPress>', onKeyPress)
 
